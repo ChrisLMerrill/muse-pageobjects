472d0c52e558957637419964ce09d29b216db7d3
ChrisLMerrill <chris@ide4selenium.com>
2020-04-08 17:11:00 -0400

    FEATURE - PerformActionStep will now only create a new variable context if running a function (not for macros). Added Return Variable parameter to allow Page actions to return a result (when calling functions)

--- src/main/java/org/museautomation/pageobject/steps/PerformActionStep.java
+++ src/main/java/org/museautomation/pageobject/steps/PerformActionStep.java
@@ -21,70 +21,79 @@ import java.util.*;
 @MuseStepTypeGroup("Page Objects")
 @MuseInlineEditString("Perform action {actionid}")
 @MuseStepShortDescription("Perform an action on a page")
-@MuseStepLongDescription("Resolve the Page source to a string and find that page in the project. Resolve the Action source and find that action on the page. Perform the action with the provided parameters.")
+@MuseStepLongDescription("Resolve the Page source to a string and find that page in the project. Resolve the Action source and find that action on the page. Perform the action with the provided parameters. The action must be a macro or function in the project")
 @MuseSubsourceDescriptor(displayName = "Page", description = "Id of the page containing the action", type = SubsourceDescriptor.Type.Named, name = PerformActionStep.PAGE_PARAM)
 @MuseSubsourceDescriptor(displayName = "Action", description = "Id of the action", type = SubsourceDescriptor.Type.Named, name = PerformActionStep.ACTION_PARAM)
 @MuseSubsourceDescriptor(displayName = "Parameters", description = "Parameters to pass to the action", type = SubsourceDescriptor.Type.Map, optional = true)
+@MuseSubsourceDescriptor(displayName = "Return variable", description = "The name of the variable to store the return value in (applies only to functions)", type = SubsourceDescriptor.Type.Named, name = CallFunction.RETURN_PARAM, optional = true)
 @MuseStepDescriptorImplementation(PerformActionDescriptor.class)
 public class PerformActionStep extends CallFunction
-	{
-	@SuppressWarnings("unused") // called via reflection
-	public PerformActionStep(StepConfiguration config, MuseProject project) throws MuseInstantiationException
-		{
-		super(config, project);
-		_page = getValueSource(config, PAGE_PARAM, true, project);
-		_action = getValueSource(config, ACTION_PARAM, true, project);
-		}
+    {
+    @SuppressWarnings("unused") // called via reflection
+    public PerformActionStep(StepConfiguration config, MuseProject project) throws MuseInstantiationException
+        {
+        super(config, project);
+        _page = getValueSource(config, PAGE_PARAM, true, project);
+        _action = getValueSource(config, ACTION_PARAM, true, project);
+        }
 
-	@Override
-	protected boolean shouldEnter(StepExecutionContext context) throws MuseExecutionError
-		{
-		boolean enter = super.shouldEnter(context);
-		if (!enter)
-			return false;
+    @Override
+    protected boolean shouldEnter(StepExecutionContext context) throws MuseExecutionError
+        {
+        boolean enter = super.shouldEnter(context);
+        if (!enter)
+            return false;
 
-		// lookup page
-		String page_id = getValue(_page, context, false, String.class);
-		Object resource = context.getProject().getResourceStorage().getResource(page_id);
-		if (!(resource instanceof WebPage))
-			throw new ValueSourceResolutionError("The Page parameter did not resolve to a WebPage in the project. Instead, it was a " + resource.getClass().getSimpleName());
-		WebPage page = (WebPage) resource;
+        // lookup page
+        String page_id = getValue(_page, context, false, String.class);
+        Object resource = context.getProject().getResourceStorage().getResource(page_id);
+        if (!(resource instanceof WebPage))
+            throw new ValueSourceResolutionError("The Page parameter did not resolve to a WebPage in the project. Instead, it was a " + resource.getClass().getSimpleName());
+        WebPage page = (WebPage) resource;
 
-		// lookup action
-		String action_id = getValue(_action, context, false, String.class);
-		PageAction action = page.getActions().get(action_id);
-		if (action == null)
-			throw new ValueSourceResolutionError("The Action parameter did not resolve to an Action on the WebPage");
+        // lookup action
+        String action_id = getValue(_action, context, false, String.class);
+        PageAction action = page.getActions().get(action_id);
+        if (action == null)
+            throw new ValueSourceResolutionError("The Action parameter did not resolve to an Action on the WebPage");
 
-		_steps_id = action.getFunction();
+        _steps_id = action.getFunction();
+        ContainsStep steps = _project.getResourceStorage().getResource(_steps_id, ContainsStep.class);
+        if (steps instanceof Function)
+            _new_scope = true;
 
-		// resolve the default parameters (sources) to be passed to the function BEFORE the new variable scope is created.
-		Map<String, ValueSourceConfiguration> sources = action.getDefaultParameters();
-		if (sources != null)
-			for (String name : sources.keySet())
-				{
-				if (name.equals(PAGE_PARAM) || name.equals(ACTION_PARAM))
-					continue;
-				resolveAndAddParameter(name, sources.get(name), context);
-				}
-		return true;
-		}
+        // resolve the default parameters (sources) to be passed to the function BEFORE the new variable scope is created.
+        Map<String, ValueSourceConfiguration> sources = action.getDefaultParameters();
+        if (sources != null)
+            for (String name : sources.keySet())
+                {
+                if (name.equals(PAGE_PARAM) || name.equals(ACTION_PARAM))
+                    continue;
+                resolveAndAddParameter(name, sources.get(name), context);
+                }
+        return true;
+        }
 
-	@NotNull
-	@Override
-	protected String getStepsId(StepExecutionContext context)
-		{
-		return _steps_id;
-		}
+    @Override
+    protected boolean isCreateNewVariableScope()
+        {
+        return _new_scope;
+        }
 
-	private final MuseValueSource _page;
-	private final MuseValueSource _action;
-	private String _steps_id;
+    @NotNull
+    @Override
+    protected String getStepsId(StepExecutionContext context)
+        {
+        return _steps_id;
+        }
 
-	public final static String PAGE_PARAM = "pageid";
-	public final static String ACTION_PARAM = "actionid";
+    private final MuseValueSource _page;
+    private final MuseValueSource _action;
+    private String _steps_id;
+    private boolean _new_scope = false;
 
-	public final static String TYPE_ID = PerformActionStep.class.getAnnotation(MuseTypeId.class).value();
-	}
+    public final static String PAGE_PARAM = "pageid";
+    public final static String ACTION_PARAM = "actionid";
 
-
+    public final static String TYPE_ID = PerformActionStep.class.getAnnotation(MuseTypeId.class).value();
+    }
